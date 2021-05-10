697d12353db1f635fadb66ca71b35d712314670c
louise-schmidt <louise-schmidt@users.noreply.github.com>
2021-05-10 17:14:55 -0400

    Entrega-parcial-1

--- src/test/java/cl/uchile/dcc/scrabble/gui/iTypeTest.java
+++ src/test/java/cl/uchile/dcc/scrabble/gui/iTypeTest.java
@@ -3,6 +3,14 @@ package cl.uchile.dcc.scrabble.gui;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class iTypeTest {
