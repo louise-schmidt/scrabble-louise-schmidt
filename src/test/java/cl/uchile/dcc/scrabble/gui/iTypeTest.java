78862736649ac0aaf46bf37c13bf0d9f4e118c2a
louise-schmidt <louise-schmidt@users.noreply.github.com>
2021-05-10 16:49:33 -0400

    Entrega-parcial-1

---
+++ src/test/java/cl/uchile/dcc/scrabble/gui/iTypeTest.java
@@ -0,0 +1,33 @@
+package cl.uchile.dcc.scrabble.gui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class iTypeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toStringS() {
+    }
+
+    @Test
+    void toBool() {
+    }
+
+    @Test
+    void toFloat() {
+    }
+
+    @Test
+    void toInt() {
+    }
+
+    @Test
+    void toBinary() {
+    }
+}
