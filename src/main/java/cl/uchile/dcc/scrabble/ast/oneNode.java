b1c12ba59e9c81573e9da38ffbdfe41dd3b29c64
louise-schmidt <louise-schmidt@users.noreply.github.com>
2021-07-05 00:25:55 -0400

    entrega2

--- src/main/java/cl/uchile/dcc/scrabble/ast/oneNode.java
+++ src/main/java/cl/uchile/dcc/scrabble/ast/oneNode.java
@@ -1,29 +1,28 @@
 package cl.uchile.dcc.scrabble.ast;
-
 import cl.uchile.dcc.scrabble.types.iType;
 
 public abstract class oneNode implements INode {
     private INode hijo;
 
     /**
-     *
-     * @param hijo
+     * Constructor oneNode
+     * @param hijo de tipo INode
      */
     public oneNode (INode hijo) {
         this.hijo = this;
     }
 
     /**
-     *
-     * @return
+     * getter nodo hijo
+     * @return INode
      */
     public INode getHijo() {
         return hijo;
     }
 
     /**
-     *
-     * @return
+     * Sobreescribe metodo evaluate de INode
+     * @return iType
      */
     @Override
     public iType evaluate() {
