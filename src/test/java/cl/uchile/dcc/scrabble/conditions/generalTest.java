3c14ddaaad7d04a04bea4f64d00cb108f9054fef
louise-schmidt <louise-schmidt@users.noreply.github.com>
2021-07-27 04:26:18 +0200

    updates T3 casi

--- src/test/java/cl/uchile/dcc/scrabble/conditions/generalTest.java
+++ src/test/java/cl/uchile/dcc/scrabble/conditions/generalTest.java
@@ -34,5 +34,26 @@ class generalTest {
         var realValueB = VarNodeFactory.getVarNode("b").getValue();
         assertEquals(expectedValueA,realValueA);
         assertEquals(expectedValueB,realValueB);
+
+        Sequence sequence2 =
+                new Sequence(
+                        new Assign("a",NodeTypeFactory.newIntNode(10)),
+                        new Assign("b", NodeTypeFactory.newIntNode(15)),
+                        new While(new EqualsTo(VarNodeFactory.getVarNode("b"), NodeTypeFactory.newIntNode(0)),
+                                new If(new not(new LessThan(VarNodeFactory.getVarNode("a"),VarNodeFactory.getVarNode("b"))),
+                                        new Assign("a",new subtract(VarNodeFactory.getVarNode("a"),VarNodeFactory.getVarNode("b"))),
+                                        new Assign("b",new subtract(VarNodeFactory.getVarNode("b"),VarNodeFactory.getVarNode("a")))
+                                )
+                        )
+                );
+
+        sequence2.lector();
+        var expectedValueA2 = NodeTypeFactory.newIntNode(10).getValue();
+        var expectedValueB2 = NodeTypeFactory.newIntNode(15).getValue();
+        var realValueA2 = VarNodeFactory.getVarNode("a").getValue();
+        var realValueB2 = VarNodeFactory.getVarNode("b").getValue();
+        assertEquals(expectedValueA2,realValueA2);
+        assertEquals(expectedValueB2,realValueB2);
     }
+
 }
